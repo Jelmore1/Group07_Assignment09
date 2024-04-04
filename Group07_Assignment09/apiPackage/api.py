2205d84acc6c47d3edbd5d462e722fe59b34b975
jakee <jakee@DESKTOP-ABODIQB>
2024-04-03 20:45:29 -0400

    New api commit

--- Group07_Assignment09/apiPackage/api.py
+++ Group07_Assignment09/apiPackage/api.py
@@ -1,5 +1,5 @@
 #api.py
-
+ 
 # Name: Jake Elmore
 # email: Elmorejc@mail.uc.edu
 # Assignment Number: Assignment 9
@@ -10,13 +10,20 @@
 # Brief Description of what this module does. Creates a class with an api
 # Citations: In class
 # Anything else that's relevant:
-
+ 
 import requests
 import json
-
-class myAPI():
-
-    response = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=SaiSUTX86CuRN2lqBeZHs5k12rGx5TyhxfnaMku5')
-    json_string = response.content
-    parsed_json = json.loads(json_string) # Now we have a python dictionary
-    
+ 
+class myAPI:
+    def __init__(self):
+        #added self so that we could get the count_photos function tied to the myAPI object
+        self.response = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=SaiSUTX86CuRN2lqBeZHs5k12rGx5TyhxfnaMku5')
+        self.json_string = self.response.content
+        self.parsed_json = json.loads(self.json_string)
+       
+    def count_photos(self):
+        '''
+        Count the number of photos in parsed_json.
+        '''
+        photos = self.parsed_json.get('photos', [])
+        return len(photos)
